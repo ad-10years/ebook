1c722e8d5dbfe15b6afcdb8528736341f354ad58
Dogfish <dyingsunlight@gmail.com>
2017-05-29 16:28:51 +0800

    update timestamp

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // webpack.config.js
 module.exports = {
@@ -8,7 +9,8 @@ module.exports = {
         common:"./src/common/entry.js"
     },
     output: {
-        filename: './docs/javascripts/[name].js'
+        path: __dirname + "/docs",
+        filename: './javascripts/[name].js'
     },
     module: {
         loaders: [
@@ -28,7 +30,19 @@ module.exports = {
         ]
     },
     plugins:[
-        new ExtractTextPlugin('./docs/stylesheets/[name].css'),
+        new ExtractTextPlugin({ filename: './stylesheets/[name].css', disable: false, allChunks: true }),
+
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/index.html', //生成的html存放路径，相对于path
+            template: __dirname + '/src/templates/index.html', //html模板路径
+            inject:  'body', //js插入的位置，true/'head'/'body'/false
+            hash: true, //为静态资源生成hash值
+            chunks: ['browser'],//需要引入的chunk，不配置就会引入所有页面的资源
+            minify: { //压缩HTML文件
+                removeComments: true, //移除HTML中的注释
+                collapseWhitespace: false //删除空白符与换行符
+            }
+        })
     ]
 };
 
