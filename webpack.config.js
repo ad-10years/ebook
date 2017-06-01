084c1a37f70e7e6f85832dd79826f278a6fe005a
Dogfish <dyingsunlight@gmail.com>
2017-06-01 21:09:54 +0800

    update innerContent to webpack generate

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
         browser:"./src/browser/entry.js",
-        common:"./src/common/entry.js"
+        innerContent:"./src/common/entry.js"
     },
     output: {
         path: __dirname + "/docs",
@@ -31,6 +31,7 @@ module.exports = {
     },
     plugins:[
         new ExtractTextPlugin({ filename: './stylesheets/[name].[hash:8].css', disable: false, allChunks: true }),
+        //browser 外围框架
         new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
             filename: __dirname + '/docs/index.html', //生成的html存放路径，相对于path
             template: __dirname + '/src/templates/index.html', //html模板路径
@@ -41,7 +42,81 @@ module.exports = {
                 removeComments: true, //移除HTML中的注释
                 collapseWhitespace: false //删除空白符与换行符
             }
-        })
+        }),
+
+        //////////////////////////////////////////////////////
+        ///////////////// 内容页面生成部分 /////////////////////
+        //////////////////////////////////////////////////////
+
+        ///////////////// 起舞弄清影 /////////////////////////
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/contents/起/十年一觉广告梦.html',
+            template: __dirname + '/src/templates/起/十年一觉广告梦.html',
+            inject:  'body',
+            hash: true,
+            chunks: ['innerContent'],
+            minify: { //压缩HTML文件
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/contents/起/封面.html',
+            template: __dirname + '/src/templates/起/封面.html',
+            inject:  'body',
+            hash: true,
+            chunks: ['innerContent'],
+            minify: { //压缩HTML文件
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/contents/起/目录.html',
+            template: __dirname + '/src/templates/起/目录.html',
+            inject:  'body',
+            hash: true,
+            chunks: ['innerContent'],
+            minify: { //压缩HTML文件
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+
+        ///////////////// 承 /////////////////////////
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/contents/承/沈伟采访.html',
+            template: __dirname + '/src/templates/承/沈伟采访.html',
+            inject:  'body',
+            hash: true,
+            chunks: ['innerContent'],
+            minify: { //压缩HTML文件
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/contents/承/周燕采访.html',
+            template: __dirname + '/src/templates/承/周燕采访.html',
+            inject:  'body',
+            hash: true,
+            chunks: ['innerContent'],
+            minify: { //压缩HTML文件
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
+            filename: __dirname + '/docs/contents/承/钟旭采访.html',
+            template: __dirname + '/src/templates/承/钟旭采访.html',
+            inject:  'body',
+            hash: true,
+            chunks: ['innerContent'],
+            minify: { //压缩HTML文件
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
     ]
 };
 
